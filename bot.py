1e50251bdba1444624e5514811839457d8cf3cfa
Nicolas Passos <nicolas.passos@amaggi.com.br>
2022-09-19 11:16:27 -0400

    update

---
+++ bot.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import teste
+
+class Telegram_Bot:
+
+    def __init__(self):
+        token = '5403052208:AAEzRFpgU64n_2Ls1BjsS0Smgare4CC6PoM'
+        self.url_base = f'https://api.telegram.org/bot{token}/'
+        self.chatter = teste.Chatter()
+        self.chatter.massive_train()
+
+    def Start(self):
+        update_id = None
+        while True:
+            atualizacao = self.get_new_messages(update_id)
+            dados = atualizacao["result"]
+            if dados:
+                for dado in dados:
+                    update_id = dado['update_id']
+                    mensagem = str(dado["message"]["text"].lower())
+                    ##usuario = str(dado["message"]["from"]["username"])
+                    chat_id = dado["message"]["from"]["id"]
+                    firstmessage = int(
+                        dado["message"]["message_id"]) == 1
+                    response = self.create_response(
+                        mensagem, firstmessage)
+                    self.responder(response, chat_id)
+
+    # Get messages
+    def get_new_messages(self, update_id):
+        link_requisicao = f'{self.url_base}getUpdates?timeout=100'
+        if update_id:
+            link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
+        resultado = requests.get(link_requisicao)
+        return json.loads(resultado.content)
+
+    # Create response
+    def create_response(self, mensagem, firstmessage):
+        message = None
+        
+        if firstmessage == True or mensagem in ('/start', '/menu'):
+            message = f'''        
+            vai a merda'''
+
+        else:
+            message = self.chatter.get_response(mensagem)
+
+        return message
+
+    # Send response
+    def responder(self, response, chat_id):
+        link_requisicao = f'{self.url_base}sendMessage?chat_id={chat_id}&text={response}'
+        requests.get(link_requisicao)
+
+if __name__ == '__main__':
+    a = Telegram_Bot()
+    a.Start()
--- teste.py
+++ teste.py
@@ -1,23 +1,52 @@
+from tkinter.filedialog import Open
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from spacy.cli import download
 
-download("en_core_web_sm")
 
-class ENGSM:
+conversa = []
+
+class ENGSM():
     ISO_639 = 'en_core_web_sm'
     ENGLISH_NAME = 'Portuguese'
+        
+class Chatter():
 
+    def __init__(self) -> None:
+        ENGSM()
+        self.chatbot = ChatBot("BotNico", tagger_language=ENGSM)
+        self.trainer = ListTrainer(self.chatbot)
 
-chatbot = ChatBot("BotNico", tagger_language=ENGSM)
+    def get_response(self, mensagem) -> str:
+        self.populate_conversa(mensagem)
+        self.train()
+        message = self.chatbot.get_response(mensagem)
+        return message
 
-conversa = [
-    "Coe",
-    "Eai, tranquilo",
-    "Tranquilo",
-]
+ 
+    def populate_conversa(self, message) -> None:
+        if message in conversa:
+            return print('Já tem, meu nobre')
+        else:
+            conversa.append(message)
+            print(f'Nova conversa inserida: {message}')
 
-trainer = ListTrainer(chatbot)
-trainer.train(conversa)
-chatbot.get_response("Coe")
-print(chatbot.get_response())
+        with open('conversa.txt', 'w') as f:
+            for i in conversa:
+                f.writelines(i + '\n')
+
+        print(f'Histórico de conversa recuperado, total: {len(conversa)}' )
+
+    def train(self):
+        self.trainer.train(conversa)
+        
+    def massive_train(self):
+        carga = []
+        with open('carga_pesada.txt', 'r') as f:
+            aux = f.readlines()
+            for i in aux:
+                print(i)
+                carga.append(i)
+        for i in carga:
+            print(i)
+            self.chatbot.get_response(i)
